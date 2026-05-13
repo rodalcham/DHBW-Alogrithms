5c8744f07d698213575db7d5a51183c3c29edd9b
Elahe Shadman <elahe.shadman@outlook.de>
2026-05-13 10:47:17 +0200

    Add function to find max and min in a list

--- Week1/FindExtremes.py
+++ Week1/FindExtremes.py
@@ -1,1 +1,14 @@
+A = [4, 2, 7, 1, 5, 9]
 
+max_val = A[0]
+min_val = A[0]
+
+for num in A:
+    if num > max_val:
+        max_val = num
+    if num < min_val:
+        min_val = num
+
+print("List:   ", A)
+print("Maximum:", max_val)
+print("Minimum:", min_val)
